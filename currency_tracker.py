87da0f9af5b8135dfeffb617e4462e1df52bf2f4
unknown <brannongarrett77@gmail.com>
2023-09-22 18:09:22 +0200

    Changed the code again

--- currency_tracker.py
+++ currency_tracker.py
@@ -1,12 +1,13 @@
 import requests
 
 
-def get_exchange_rate(base_currency, target_currency):
-    url = f"https://api.exchangeratesapi.io/latest?base={base_currency}&symbols={target_currency}"
+def get_exchange_rate(base_currency, target_currency, api_key):
+    url = f"https://openexchangerates.org/api/latest.json?app_id={api_key}&base={base_currency}&symbols={target_currency}"
     response = requests.get(url)
     data = response.json()
     return data["rates"][target_currency]
 
 
-eur_to_usd = get_exchange_rate("EUR", "USD")
+api_key = "e03da58520764a309704cb57e64dee00"
+eur_to_usd = get_exchange_rate("EUR", "USD", api_key)
 print(f"1 EUR = {eur_to_usd} USD")
