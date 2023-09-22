6e3795eeee9c48d2beba77632e1735751090c2e7
unknown <brannongarrett77@gmail.com>
2023-09-22 17:58:29 +0200

    Reverted back to the original code

--- currency_tracker.py
+++ currency_tracker.py
@@ -1,5 +1,3 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton
 import requests
 
 
@@ -10,24 +8,5 @@ def get_exchange_rate(base_currency, target_currency):
     return data["rates"][target_currency]
 
 
-def update_exchange_rate():
-    eur_to_usd = get_exchange_rate("EUR", "USD")
-    exchange_rate_label.setText(f"1 EUR = {eur_to_usd:.2f} USD")
-
-
-app = QApplication(sys.argv)
-window = QWidget()
-layout = QVBoxLayout()
-
-exchange_rate_label = QLabel()
-update_exchange_rate()
-
-update_button = QPushButton("Update Exchange Rate")
-update_button.clicked.connect(update_exchange_rate)
-
-layout.addWidget(exchange_rate_label)
-layout.addWidget(update_button)
-
-window.setLayout(layout)
-window.show()
-sys.exit(app.exec_())
+eur_to_usd = get_exchange_rate("EUR", "USD")
+print(f"1 EUR = {eur_to_usd} USD")
